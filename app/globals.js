1a2a9b219a38703bf9486a1480bd89babd60f416
Jamyye <jfish132@gmail.com>
2018-10-08 22:15:31 +0000

    first

--- app/globals.js
+++ app/globals.js
@@ -4,9 +4,9 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "XMRPool.net",
-		api_url : 'https://api.xmrpool.net',
-		api_refresh_interval: 5000,
-		app_update_interval: 5*60000
+		pool_name: "aeon.privpool.com",
+		api_url : 'https://aeon-api.privpool.com',
+		api_refresh_interval: 30000,
+		app_update_interval: 10*60000
 	};
 });
